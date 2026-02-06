3720462acdf76bd32ad160edaf706a18f5624c12
JB1574 <mrln0367@leeds.ac.uk>
2026-02-06 10:35:22 +0000

    End of session2 week 2

--- session_2/base.py
+++ session_2/base.py
@@ -18,6 +18,39 @@ def main():
 
     db.close()
 
+def list_product_categories(db):
+    query = '''
+            SELECT category,product_id FROM 
+            products
+            '''
+    cursor = db.execute(query)
+    for category in products:
+         print(f"ID: {catgegory[0]}\tName: {category[1]}")
+    
+def count_total_customers(db):
+    query = '''
+            SELECT COUNT(customer_id) AS total_customers
+            FROM customers
+            '''
+    cursor = db.execute(query)
+    for customer_id in cursor:
+        Print(f"Total customers  ")
 
+def show_orders_per_customer(db):
+    choice = -1
+    while(choice < 1 ):
+        choice = input("Enter customer email: ")
+    try:
+        choice = input(choice)
+    except:
+        choice =-1
+
+    query = '''
+            SELECT orders.order_id FROM
+            orders
+            JOIN customers ON orders.customer_id = customers .customer_id
+            '''
+        cursor =db.execute(query,(choice,))
+def displa
 if __name__=="__main__":
     main()
